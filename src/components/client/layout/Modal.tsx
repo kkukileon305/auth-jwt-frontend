12a1e8e04c185880acb53ae52caf8aee5a03884c
kkukileon305 <kkukileon305@ofotd.com>
2023-05-24 14:28:40 +0900

    modify: change onClose handler

--- src/components/client/layout/Modal.tsx
+++ src/components/client/layout/Modal.tsx
@@ -1,15 +1,37 @@
 'use client';
 
 import { useModalStore } from '@/store/modal.store';
+import { MouseEventHandler } from 'react';
 
 const Modal = () => {
   const { setModal, modal } = useModalStore();
+
+  const handleClose: MouseEventHandler<HTMLElement> = (event) => {
+    event.preventDefault();
+
+    if (event.target === event.currentTarget) {
+      setModal(null);
+
+      if (modal?.onConfirm) {
+        modal.onConfirm();
+      }
+    }
+  };
+
+  const handleConfirm = () => {
+    setModal(null);
+
+    if (modal?.onConfirm) {
+      modal.onConfirm();
+    }
+  };
 
   return (
     <div
       className={`${
         modal ? 'fixed' : 'hidden'
       } w-full h-full z-10 bg-black/50 left-0 top-0 flex justify-center items-center p-4`}
+      onClick={handleClose}
     >
       <div className='bg-white max-w-xl w-full flex flex-col gap-8 shadow-xl'>
         <div className='flex flex-col gap-8 p-4'>
@@ -18,9 +40,9 @@ const Modal = () => {
         </div>
         <button
           className={`w-full bg-blue-400 px-8 py-2 mx-auto text-white font-bold
-          ${modal?.type ? 'bg-red-400' : 'bg-blue-400'}
+          ${modal?.type === 'error' ? 'bg-red-400' : 'bg-blue-400'}
           `}
-          onClick={() => setModal(null)}
+          onClick={handleConfirm}
         >
           Close
         </button>
